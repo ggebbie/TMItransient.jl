664298cabb06182cd361c879917fad6b49b3d8ff
G Jake Gebbie <ggebbie@whoi.edu>
2023-05-17 10:11:35 -0400

    watermass_stepresponse for surface response
    also add scripts env

--- scripts/watermass_stepresponse.jl
+++ scripts/watermass_stepresponse.jl
@@ -22,64 +22,44 @@ using Revise, TMI
 TMIversion = "modern_90x45x33_GH10_GH12"
 A, Alu, γ, TMIfile, L, B = config_from_nc(TMIversion);
 
-#In MATLAB code: make_initial_condition 
-latbox = [50,60]
-lonbox = [-50,0]
-d = surfacepatch(lonbox, latbox, γ) #vector(same dims as γ, global)  describing surface patch in terms of γ
+# read a water-mass surface patch from these choices
+list = ("GLOBAL","ANT","SUBANT",
+            "NATL","NPAC","TROP","ARC",
+            "MED","ROSS","WED","LAB","GIN",
+            "ADEL","SUBANTATL","SUBANTPAC","SUBANTIND",
+            "TROPATL","TROPPAC","TROPIND")
 
-#flatten d and access at surface wet positions 
-dsfc = d.tracer[d.wet]
+# choose water mass (i.e., surface patch) of interest
+region = list[1]
 
-#initial conditions are the surface patch = 1, propagated down to the bottom of the mixed layer, which we get from B 
-c0 = B * dsfc 
+# do numerical analysis
+b = TMI.surfaceregion(TMIversion,region,γ)
 
-#Fixed euler timestep approximation
-c = c0
-Δt = 1e-3 #this becomes unstable if you go any lower
-for tt = 1:20
-    # forward Euler timestep
-    global c += L*c*Δt
-    println("Sum c = " *string(sum(c)) *", initial c = " *string(sum(c0)))
-end
+# preallocate initial condition Field
+c₀ = zeros(γ)
 
-#make_boundary_conditions: can be "fixed" or "varying"
-bc = "varying" #"fixed" or "varying"
-u0 = c0
-du = similar(u0)
+# update d with the boundary condition b
+#setboundarycondition!(c,b)
+
+c₀.tracer[TMI.wet(c₀)] = B* b.tracer[b.wet]
+#initial conditions are the surface patch = 1, propagated down to the bottom of the mixed layer, which we get from B
+#c0 = B * dsfc 
+
+u0 = c₀
+du = zeros(γ)
 tspan = (0.0, 50.0)
 
 #Solving differential equation
 #NOTE: for DifferentialEquations.jl to work, follow naming conventions in docs
-if bc == "fixed" 
-    f(du,u,p,t) = mul!(du, L, u) #this avoids allocating a new array for each iteration
 
-elseif bc == "varying"
-    #example conditions
-    #at t = 0, surface = 1. at t = 50, surface = 0.
-    #i.e., surface values linearly decrease from 1 to 0 over the span of 50 years 
-    tsfc = [0, 50]
-    Csfc = zeros((2, length(dsfc)))
-    Csfc[1, :] .= 1
-    τ = 1 / 12 #monthly restoring timescale
-    li = LinearInterpolation(tsfc, 1:length(tsfc))
-
-    #Instantiate arrays that the diffeq solver will reallocate
-    LC = DiffEqBase.dualcache(similar(u0)) #for PreallocationTools.jl
-    BF = DiffEqBase.dualcache(similar(u0)) #for PreallocationTools.jl 
-    Cb = similar(Csfc[1,:])
-    surface_ind = findall(x-> x[3] == 1, γ.I)#which points in γ.I are on the surface  
-    
-    p = (Csfc, surface_ind, τ,L,B, li, LC,BF, Cb) #parameters 
-    f(du, u, p, t) = varying!(du, u, p, t) #ODEfunction 
-else
-    println("invalid boundary condition, must be 'fixed' or 'varying'")
-end
+f(du,u,p,t) = mul!(du, L, u) #this avoids allocating a new array for each iteration
 
 #Solve diff eq
 operator = DiffEqArrayOperator(L)
 #isconstant(operator) # not currently working
 func = ODEFunction(f, jac_prototype = L) #jac_prototype for sparse array 
-prob = ODEProblem(func, u0, tspan,p)
+prob = ODEProblem(func, u0, tspan)
+#prob = ODEProblem(func, u0, tspan,p)
 println("Solving ode")
 #solve using QNDF alg - tested against other alg and works fastest 
 @time sol = solve(prob,QNDF(),abstol = 1e-4,reltol=1e-4,saveat =tspan[1]:tspan[2])
@@ -103,3 +83,9 @@ lims = 0:0.05:3.0
 snapshot = TMI.Field(sol_array[25,:,:,:],γ)
 label = "Some quantity [units]"
 sectionplot(snapshot,lon_section,lims,titlelabel=label)
+
+# plot a section at 330 east longitude (i.e., 30 west)
+lon_section = 330 # only works if exact
+lims = 0:5:100
+tlabel = region * " water-mass fraction [%]"
+sectionplot(100g,lon_section,lims,titlelabel = tlabel)
